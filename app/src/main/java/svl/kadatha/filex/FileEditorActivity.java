53cd2bd64f2ea2dcb31a5eba9c9b047cfba8efdd
shankarvl1980 <shankar_vl@rediffmail.com>
2024-08-25 18:42:52 +0530

    c

--- app/src/main/java/svl/kadatha/filex/FileEditorActivity.java
+++ app/src/main/java/svl/kadatha/filex/FileEditorActivity.java
@@ -1087,8 +1087,8 @@ public class FileEditorActivity extends BaseActivity implements FileEditorSettin
 								{
 									save_button.setEnabled(false);
 									save_button.setAlpha(Global.DISABLE_ALFA);
-									viewModel.current_page_end_point=fileSaveService2.page_pointer_hashmap.get(viewModel.current_page);
-									viewModel.page_pointer_hashmap=fileSaveService2.page_pointer_hashmap;
+									viewModel.current_page_end_point=fileSaveService2.pagePointerHashmap.get(viewModel.current_page);
+									viewModel.page_pointer_hashmap=fileSaveService2.pagePointerHashmap;
 									viewModel.updated=result;
 
 									if(result)
